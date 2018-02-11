3956fb7609716f8aaadaa30b59b731bc03bbb17a
Jesse Stuart <hi@jessestuart.com>
2018-02-11 08:00:55 -0500

    [config] Split colors out into a separate module.

---
+++ src/config/module-colors.js
@@ -0,0 +1,103 @@
+//===========================================
+// Shades of gray. These have no hue or tint.
+//===========================================
+const neutralShadesOfBlackAndWhite = {
+  black: '#000',
+  'near-black': '#111',
+  'dark-gray': '#333',
+  'mid-gray': '#555',
+  gray: '#777',
+  silver: '#999',
+  'light-silver': '#aaa',
+  'moon-gray': 'rgba(220, 220, 220, 0.9)',
+  'light-gray': '#eee',
+  'near-white': '#f4f4f4',
+  white: '#fff',
+}
+
+//==============================================
+// Solid black with varying translusency
+//==============================================
+const translucentBlacks = {
+  'black-90': 'rgba(0, 0, 0, 0.9)',
+  'black-80': 'rgba(0, 0, 0, 0.8)',
+  'black-70': 'rgba(0, 0, 0, 0.7)',
+  'black-60': 'rgba(0, 0, 0, 0.6)',
+  'black-50': 'rgba(0, 0, 0, 0.5)',
+  'black-40': 'rgba(0, 0, 0, 0.4)',
+  'black-30': 'rgba(0, 0, 0, 0.3)',
+  'black-20': 'rgba(0, 0, 0, 0.2)',
+  'black-10': 'rgba(0, 0, 0, 0.1)',
+  'black-05': 'rgba(0, 0, 0, 0.05)',
+  'black-025': 'rgba(0, 0, 0, 0.025)',
+  'black-0125': 'rgba(0, 0, 0, 0.0125)',
+}
+
+//==============================================
+// Solid white with varying translusency
+//==============================================
+const translucentWhites = {
+  'white-90': 'rgba(255, 255, 255, 0.9)',
+  'white-80': 'rgba(255, 255, 255, 0.8)',
+  'white-70': 'rgba(255, 255, 255, 0.7)',
+  'white-60': 'rgba(255, 255, 255, 0.6)',
+  'white-50': 'rgba(255, 255, 255, 0.5)',
+  'white-40': 'rgba(255, 255, 255, 0.4)',
+  'white-30': 'rgba(255, 255, 255, 0.3)',
+  'white-20': 'rgba(255, 255, 255, 0.2)',
+  'white-10': 'rgba(255, 255, 255, 0.1)',
+  'white-05': 'rgba(255, 255, 255, 0.05)',
+  'white-025': 'rgba(255, 255, 255, 0.025)',
+  'white-0125': 'rgba(255, 255, 255, 0.0125)',
+}
+
+const colors = {
+  ...neutralShadesOfBlackAndWhite,
+  ...translucentBlacks,
+  ...translucentWhites,
+
+  //=========================================================
+  // Special "brand" shades of gray -- note that these aren't
+  // pure grays.
+  //=========================================================
+  'gray-primary': 'rgb(55, 59, 70)',
+  'gray-primary-darker': 'rgb(41, 44, 52)',
+
+  //====================
+  // Primary brand colors.
+  //====================
+  'hot-pink': '#ff3a5c',
+  purple: '#673ab7',
+  'light-purple': 'rgb(199, 153, 255)',
+  blue: '#56e2ff',
+  green: '#b4df75',
+
+  //=================
+  // These need work.
+  //=================
+  navy: '#001b44',
+  'dark-blue': '#00449e',
+  'dark-green': '#41D69F',
+  'light-blue': '#96ccff',
+  'lightest-blue': '#cdecff',
+  'dark-pink': '#d5008f',
+  'dark-red': '#e7040f',
+  'washed-green': '#e8fdf5',
+  'light-pink': '#efa4b8',
+  'light-yellow': '#f3dd70',
+  pink: '#f49cc8',
+  'washed-blue': '#f6fffe',
+  'light-yellow': '#fbeba9',
+  red: '#ff4136',
+  orange: '#ff6300',
+  'light-red': '#ff725c',
+  gold: '#ffb700',
+  'light-red': '#ffd3c0',
+  yellow: '#ffde37',
+  'washed-red': '#ffdfdf',
+  'washed-yellow': '#fffceb',
+
+  transparent: 'transparent',
+}
+
+module.exports = colors
--- src/config/tachyons-definition.js
+++ src/config/tachyons-definition.js
@@ -1,86 +1,10 @@
-module.exports = {
-  // fs1 => fs7
-  typeScale: [3, 2, 1.5, 1.25, 1, 0.875, 0.75],
-  spacing: [0.25, 0.5, 1, 1.75, 2.5, 8, 16],
-  // Alternatively: 45 & 60 (?)
-  customMedia: [{ ns: { value: 45, minWidth: true } }],
-  colors: {
-    //================
-    // Shades of gray.
-    //================
-    black: '#000',
-    'near-black': '#111',
-    'dark-gray': '#333',
-    'mid-gray': '#555',
-    gray: '#777',
-    silver: '#999',
-    'light-silver': '#aaa',
-    'moon-gray': 'rgba(220, 220, 220, 0.9)',
-    'light-gray': '#eee',
-    'near-white': '#f4f4f4',
-    white: '#fff',
-    transparent: 'transparent',
-    //==============================================
-    // Solid black / white with varying translusency
-    //==============================================
-    'black-90': 'rgba(0, 0, 0, 0.9)',
-    'black-80': 'rgba(0, 0, 0, 0.8)',
-    'black-70': 'rgba(0, 0, 0, 0.7)',
-    'black-60': 'rgba(0, 0, 0, 0.6)',
-    'black-50': 'rgba(0, 0, 0, 0.5)',
-    'black-40': 'rgba(0, 0, 0, 0.4)',
-    'black-30': 'rgba(0, 0, 0, 0.3)',
-    'black-20': 'rgba(0, 0, 0, 0.2)',
-    'black-10': 'rgba(0, 0, 0, 0.1)',
-    'black-05': 'rgba(0, 0, 0, 0.05)',
-    'black-025': 'rgba(0, 0, 0, 0.025)',
-    'black-0125': 'rgba(0, 0, 0, 0.0125)',
-    'white-90': 'rgba(255, 255, 255, 0.9)',
-    'white-80': 'rgba(255, 255, 255, 0.8)',
-    'white-70': 'rgba(255, 255, 255, 0.7)',
-    'white-60': 'rgba(255, 255, 255, 0.6)',
-    'white-50': 'rgba(255, 255, 255, 0.5)',
-    'white-40': 'rgba(255, 255, 255, 0.4)',
-    'white-30': 'rgba(255, 255, 255, 0.3)',
-    'white-20': 'rgba(255, 255, 255, 0.2)',
-    'white-10': 'rgba(255, 255, 255, 0.1)',
-    'white-05': 'rgba(255, 255, 255, 0.05)',
-    'white-025': 'rgba(255, 255, 255, 0.025)',
-    'white-0125': 'rgba(255, 255, 255, 0.0125)',
-    //====================
-    // Primary-ish colors.
-    //====================
-    'hot-pink': '#ff3a5c',
-    purple: '#673ab7',
-    'light-purple': 'rgb(199, 153, 255)',
-    blue: '#56e2ff',
-    green: '#41D69F',
+const colors = require('./module-colors')
 
-    //=================
-    // These need work.
-    //=================
-    'dark-red': '#e7040f',
-    red: '#ff4136',
-    'light-red': '#ff725c',
-    orange: '#ff6300',
-    gold: '#ffb700',
-    yellow: '#ffde37',
-    'light-yellow': '#fbeba9',
-    'dark-pink': '#d5008f',
-    pink: '#f49cc8',
-    'dark-green': '#006C71',
-    navy: '#001b44',
-    'dark-blue': '#00449e',
-    'light-blue': '#96ccff',
-    'lightest-blue': '#cdecff',
-    'light-pink': '#efa4b8',
-    'light-yellow': '#f3dd70',
-    'light-red': '#ffd3c0',
-    'washed-blue': '#f6fffe',
-    'washed-green': '#e8fdf5',
-    'washed-yellow': '#fffceb',
-    'washed-red': '#ffdfdf',
-  },
+module.exports = {
+  ...colors,
+  typeScale: [3, 2.25, 1.75, 1.15, 1, 0.875, 0.75],
+  spacing: [0.25, 0.5, 1, 1.75, 2.5, 8, 16],
+  customMedia: [{ ns: { value: 45, minWidth: true } }],
   borderWidths: [0, 0.125, 0.25, 0.5, 1, 2],
   borderRadius: [0, 0.125, 0.25, 0.5, 1],
   widths: [1, 2, 4, 8, 16],
@@ -90,8 +14,5 @@ module.exports = {
     measure: [30, 34, 20],
   },
   opacity: [1, 0.9, 0.8, 0.7, 0.6, 0.5, 0.4, 0.3, 0.2, 0.1, 0.05, 0.025, 0],
-  lhSolid: 1.25,
-  lhTitle: 1.5,
-  lhCopy: 1.7,
   lineHeights: [1.7, 1.5, 1.25],
 }
