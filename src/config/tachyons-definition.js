204c776d1dbe79405dc05c6decb3fb23df9ca4bb
Jesse Stuart <hi@jessestuart.com>
2018-02-12 06:18:33 -0500

    [style] Update to use forked tachyons-generator supporting line-heights.

--- src/config/tachyons-definition.js
+++ src/config/tachyons-definition.js
@@ -4,6 +4,7 @@ module.exports = {
   colors,
   typeScale: [3, 2.25, 1.75, 1.15, 1, 0.875, 0.75],
   spacing: [0.25, 0.5, 1, 1.75, 2.5, 8, 16],
+  lineHeight: [1.25, 1.5, 1.7],
   customMedia: [{ ns: { value: 45, minWidth: true } }],
   borderWidths: [0, 0.125, 0.25, 0.5, 1, 2],
   borderRadius: [0, 0.125, 0.25, 0.5, 1],
@@ -14,5 +15,4 @@ module.exports = {
     measure: [30, 34, 20],
   },
   opacity: [1, 0.9, 0.8, 0.7, 0.6, 0.5, 0.4, 0.3, 0.2, 0.1, 0.05, 0.025, 0],
-  lineHeights: [1.7, 1.5, 1.25],
 }
